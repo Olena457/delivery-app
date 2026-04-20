b4b1f8792a2f4fe69ff0cbb054ab5398b52f72c9
Olena457 <olenademianenko42@gmail.com>
2026-04-20 22:34:51 +0300

    Update telegram.module.ts

--- apps/server/src/telegram/telegram.module.ts
+++ apps/server/src/telegram/telegram.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TelegramUpdate } from './telegram.update';
 import { AiModule } from '../ai/ai.module';
-import { PrismaModule } from 'src/common/prisma/prisma.module';
 
 @Module({
-  imports: [AiModule, PrismaModule],
+  imports: [AiModule],
   providers: [TelegramUpdate],
 })
 export class TelegramModule {}
