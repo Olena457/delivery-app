abf96c363a6020c98bd99647d8f75bfdbfbc5af2
Olena457 <olenademianenko42@gmail.com>
2026-04-23 15:37:18 +0300

    Update main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -30,7 +30,8 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
+      forbidNonWhitelisted: false,
+      // forbidNonWhitelisted: true,
       transform: true,
       transformOptions: { enableImplicitConversion: true },
     }),
