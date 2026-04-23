60450d6f6df146b9976037253ac03169e73982b0
Olena457 <olenademianenko42@gmail.com>
2026-04-23 14:37:15 +0300

    Update telegram.controller.ts

--- apps/server/src/telegram/telegram.controller.ts
+++ apps/server/src/telegram/telegram.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
-
+import { TelegramUpdateDto } from './dto/telegram.dto';
 @Controller('webhook/telegram')
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) {}
 
   @Post()
   @HttpCode(200)
-  async receiveUpdate(@Body() update: any) {
-    return this.telegramService.handleUpdate(update);
+  async receiveUpdate(@Body() update: TelegramUpdateDto) {
+    return this.telegramService.processUpdate(update);
   }
 }
